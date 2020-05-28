684c8bbc09b6803f89dd3c36126f414eb7473107
Kat <katherinearias@users.noreply.github.com>
2020-05-27 23:00:16 -0400

    fix

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
-console.log("Hey Ya'll!")
+console.log("I done did it!")
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            }
+            else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+                console.log(index / 7);
+            }
+
+        });
+
+        burger.classList.toggle('toggle');
+    });
+
+}
+
+navSlide();
+
+
 
 const navSlide = () => {
     const burger = document.querySelector('.burger');
@@ -31,4 +60,5 @@ function fade (){
     $("#preloader").fadeOut("slow");
 }
 
-setTimeout (fade, 2000)
+setTimeout (fade, 3000)
+
